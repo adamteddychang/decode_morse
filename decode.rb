ad9c9a10b9185a643b87ae643fdf43fd0de447d1
Taher <ttaherhagui@gmail.com>
2022-01-19 13:43:57 +0100

    add decode file .

---
+++ decode.rb
@@ -0,0 +1,18 @@
+require_relative './morse.rb'
+
+def decode_word(word)
+  arr_word = word.split(' ')
+  result = ''
+  arr_word.each do |char| 
+    result += decodechar(char)
+  end
+  return result
+  end 
+def decode_message(message)
+  result = ''
+  arr_message = message.split('   ')
+  arr_message.each { |word| result += " #{decode_word(word)}"}
+  return result
+  end
+
+puts  decode_message(".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-. / .-. ..- -... .. . ...")
